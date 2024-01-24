92c4efe0821681bb77922f3f2d840a9096fc527f
motonim <motonimjrp@gmail.com>
2024-01-24 17:08:31 -0500

    creating a tag route

---
+++ src/views/Tag.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="tag">
+        <div v-if="error">{{  error }}</div>
+        <div v-if="posts.length">
+            <PostList :posts="postsWithTag" />
+        </div>
+        <div v-else>
+            <Spinner />
+        </div>
+    </div>
+
+</template>
+
+<script>
+import getPosts from '@/composables/getPosts';
+import Spinner from '@/components/Spinner.vue';
+import PostList from '@/components/PostList.vue';
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+export default {
+    components: { Spinner, PostList },
+    setup() {
+        const route = useRoute()
+        const { posts, error, load } = getPosts()
+        load();
+        // console.log(posts);
+
+        // console.log(props.tag)
+        const postsWithTag = computed(() => {
+            return posts.value.filter(post => 
+                post.tags.includes(route.params.tag)
+            )
+        })
+
+        return { error, posts, postsWithTag }
+    }
+}
+</script>
