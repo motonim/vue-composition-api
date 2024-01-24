07e2c4dc60b139728658cd040241cd1ba7564be7
motonim <motonimjrp@gmail.com>
2024-01-24 15:31:23 -0500

    add Navbar

--- src/composables/getPosts.js
+++ src/composables/getPosts.js
@@ -7,9 +7,9 @@ const getPosts = () => {
     const load = async() => {
       try {
         //simulate delay
-          await new Promise(resolve => {
-            setTimeout(resolve, 2000)
-        })
+        //   await new Promise(resolve => {
+        //     setTimeout(resolve, 2000)
+        // })
         
         let data = await fetch('http://localhost:3000/posts')
         // console.log(data)
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <!-- <h1>Home</h1>
     <p ref="p">My name is {{  name  }} and I'm {{  age  }} years old.</p>
     <button @click="handleName">Click me</button>
     <button @click="age++">add 1 to age</button>
@@ -14,7 +14,7 @@
       <button @click="handleClick">stop watching</button>
     </div>
 
-    <hr>    
+    <hr>     -->
     <div v-if="posts.length">
       <PostList v-if="showPosts" :posts="posts" />
     </div>
@@ -37,44 +37,44 @@ export default {
   name: 'HomeView',
   components: { PostList, Spinner },
   setup() {
-    const p = ref(null)
-    // console.log(p, p.value)
+    // const p = ref(null)
+    // // console.log(p, p.value)
 
-    // const name = 'Mario'
-    // const age = 39
-    const name = ref('Mario')
-    const age = ref(39)
+    // // const name = 'Mario'
+    // // const age = 39
+    // const name = ref('Mario')
+    // const age = ref(39)
 
-    const handleName = () => {
-      // console.log(p, p.value)
-      // p.value.classList.add('test')
-      // p.value.textContent = 'It has changed'
-      // name = 'Luigi' // it won't change the name because the const name is not reactive inside setup. we need to use ref
-      name.value = 'Luigi'
-    }
+    // const handleName = () => {
+    //   // console.log(p, p.value)
+    //   // p.value.classList.add('test')
+    //   // p.value.textContent = 'It has changed'
+    //   // name = 'Luigi' // it won't change the name because the const name is not reactive inside setup. we need to use ref
+    //   name.value = 'Luigi'
+    // }
 
-    // ------------------------------
+    // // ------------------------------
 
-    const search = ref('')
-    const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
+    // const search = ref('')
+    // const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
 
-    const matchingNames = computed(() => {
-      return names.value.filter(name => name.includes(search.value))
-    })
+    // const matchingNames = computed(() => {
+    //   return names.value.filter(name => name.includes(search.value))
+    // })
 
-    const stopWatch = watch(search, () => {
-      console.log('watch function ran')
-    })
+    // const stopWatch = watch(search, () => {
+    //   console.log('watch function ran')
+    // })
 
-    const stopEffect = watchEffect(() => {
-      console.log('watchEffect ran', search.value)
-      console.log(names.value)
-    })
+    // const stopEffect = watchEffect(() => {
+    //   console.log('watchEffect ran', search.value)
+    //   console.log(names.value)
+    // })
 
-    const handleClick = () => {
-      stopWatch()
-      stopEffect()
-    }
+    // const handleClick = () => {
+    //   stopWatch()
+    //   stopEffect()
+    // }
 
     // -------------------------------
 
@@ -83,7 +83,8 @@ export default {
     const { posts, error, load } = getPosts()
     load();
     
-    return { name, names, age, handleName, p, search, matchingNames, handleClick, posts, showPosts, error }
+    // return { name, names, age, handleName, p, search, matchingNames, handleClick, posts, showPosts, error }
+    return { posts, showPosts, error }
 
     // return { name, age, handleClick, p}
   }
